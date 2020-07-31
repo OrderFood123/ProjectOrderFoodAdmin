07a299beb5d0ad7c325acd769f2340f2a16639f5
HienPhuong99 <60739721+HienPhuong99@users.noreply.github.com>
2020-07-31 17:28:06 +0700

    Hoàn thiện đăng nhập Admin

---
+++ orderfoodadmin/app/src/main/java/com/example/orderfoodadmin/LoginActivity.java
@@ -0,0 +1,87 @@
+package com.example.orderfoodadmin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.orderfoodadmin.model.Admin;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etPhoneNumber, etPassword;
+    Button btnLogin;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etPhoneNumber = findViewById(R.id.etPhoneNumber);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        firebaseDatabase = firebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("admin");
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginAdmin(etPhoneNumber.getText().toString(), etPassword.getText().toString());
+            }
+        });
+
+    }
+
+    private void loginAdmin(final String phoneNumber, String passowrd) {
+        Context context;
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setMessage("Đang lấy dữ liệu...");
+        progressDialog.show();
+
+        final String localPhonenumber = phoneNumber;
+        final String localPassword = passowrd;
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(localPhonenumber).exists()) {
+                    progressDialog.dismiss();
+                    Admin admin = snapshot.child(localPhonenumber).getValue(Admin.class);
+                    admin.setPhoneNumber(localPhonenumber);
+                    if (admin.getPassword_admin().equals(localPassword)) {
+                        Intent iLogin = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(iLogin);
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Đăng nhập không thành công!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this, "Số điện thoại không tồn tại!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
